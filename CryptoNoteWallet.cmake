eba86075fb1c72608920c23346a595cafd92dfa3
samvicky <billavignesh26@gmail.com>
2018-03-20 13:03:08 +0530

    First version

---
+++ CryptoNoteWallet.cmake
@@ -0,0 +1,5 @@
+
+set(CN_PROJECT_NAME "worktips")
+set(CN_CURRENCY_DISPLAY_NAME "WorkTips")
+set(CN_CURRENCY_TICKER "WTIP")
+set(CN_VERSION "1.0.0")
---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x54, 0x32, 0x55, 0xc7, 0xf1, 0xdf, 0x2c, 0x7f, 0xf8, 0x27, 0x29, 0xa9, 0xc3, 0x71, 0xf4, 0x37 } };
+}
